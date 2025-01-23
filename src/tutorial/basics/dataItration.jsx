1c5991f194bbf87ca2fae7074b39978b96fce714
abin c joseph <abincjoseph77@gmail.com>
2025-01-23 20:22:01 +0530

    UseState-itrations and remoeve item

--- src/tutorial/basics/dataItration.jsx
+++ src/tutorial/basics/dataItration.jsx
@@ -2,15 +2,27 @@ import { data } from "../../data";
 import React from "react";
 const UseStateArry = ()=>{
     let [people,setArry] = React.useState(data);
+
+    const removeItem = (id)=>{
+        const newPeopl = people.filter((person)=>person.id !== id);
+        setArry(newPeopl);
+    }
+
+    const ClearAllItem = ()=>{
+    setArry([]);
+    }
 return(
+
     <>
     {people.map((person)=>{
         console.log(person)
         const {id,name} = person;
         return <div key={id}>
             <h4>{name}</h4>
+            <button type="submit" onClick={()=>removeItem(id)}>remove</button>
         </div>;
     })}
+    <button type="submit" onClick={ClearAllItem} style={{marginTop:'2rem'}}>Cleare</button>
     </>
 )
 }
