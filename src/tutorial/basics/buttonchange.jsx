1059c40366c7d6d70c80981a37d05da67968699b
abin c joseph <abincjoseph77@gmail.com>
2025-01-23 22:01:32 +0530

    btn change 2

--- src/tutorial/basics/buttonchange.jsx
+++ src/tutorial/basics/buttonchange.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 
 function ChangeValue(){
-    const [name,setName] = useState('abin');
-    const [age,setAg] = useState(25);
-    const [hobby,setHobby] = useState('nill');
-    
+    const [person,setPerson] = useState({
+        name:'abin',
+        age:25,
+        hobby:'nill'
+    })
+ 
     const Handleclick = ()=>{
-        setName('sabin');
-        setAg(20);
-        setHobby('nothig')
+setPerson({name:'sabin',age:20,hobby:'streming'});
     };
 
     return(
         <>
-        <h1>name:{name}</h1>
-        <h1>age:{age}</h1>
-        <h1>hobby:{hobby}</h1>
+        <h1>name:{person.name}</h1>
+        <h1>age:{person.age}</h1>
+        <h1>hobby:{person.hobby}</h1>
         <button onClick={Handleclick} type="button">click me</button>
         </>
     );
