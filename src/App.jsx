96b2a1c9ee00a86baa453a43af50a720f9e9f5d4
abin c joseph <abincjoseph77@gmail.com>
2025-01-26 16:15:03 +0530

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 // import ChangeValue from './tutorial/basics/buttonchange'
-import IncreaseSetValue from './tutorial/basics/increaseSateVslue'
+// import IncreaseSetValue from './tutorial/basics/increaseSateVslue'
+import ToDo from './tutorial/basics/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
-       <IncreaseSetValue/>
+       <ToDo/>
       </>
   );
 };
---
+++ src/tutorial/basics/Todo.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function ToDo(){
+
+    const [value,setValue] = useState('')
+    const [arr,Setarr] = useState([])
+
+    const handleClick = (evt)=>{
+        setValue(evt.target.value);
+    }
+
+    const handleAdd = ()=>{
+        if(value){
+            const Newvalue = {id:arr.length+1,list:value}
+            Setarr([...arr,Newvalue]);
+            setValue()
+        }
+    }
+
+    const RemoveItem = (id)=>{
+        const NewList = arr.filter((item)=> item.id !== id)
+        Setarr(NewList)
+    }
+
+    return(
+        <>
+        <div className="">
+            <input value={value} type="text"  onChange={handleClick}/>
+            <button onClick={handleAdd}>add</button>
+            <div className="">
+                {arr.map((item)=>(
+                    <div key={item.id}>
+                        <h1>{item.list}</h1>
+                        <button onClick={()=>RemoveItem(item.id)}>remove</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+        </>
+    );
+};
+export default ToDo;
